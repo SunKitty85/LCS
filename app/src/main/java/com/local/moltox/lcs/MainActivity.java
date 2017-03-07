db29d636d38c7b35b757a501703e48ba9663f0df
moltox <masterm_mh@hotmail.de>
2017-03-07 09:38:37 +0100

    170307-0925_01

---
+++ app/src/main/java/com/local/moltox/lcs/MainActivity.java
@@ -0,0 +1,73 @@
+package com.local.moltox.lcs;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    View view;
+    int count;
+    Button m_btn_continue;
+    TextView m_tv_main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        count = 0;
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        m_btn_continue = (Button)  findViewById(R.id.btn_continue);
+        m_tv_main = (TextView) findViewById(R.id.id_tv_main);
+        m_btn_continue.setOnClickListener(new View.OnClickListener()  {
+            public void onClick(View v)  {
+                callQandaActivity(v);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void callQandaActivity(View view)  {
+        Intent intent = new Intent(this,qanda_activity.class);
+        startActivity(intent);
+    }
+}
